c9cfde77cb9565e2ff9f3c4505225cfdf488ff64
Priyanka <priyoo.badgujar@gmail.com>
2020-09-04 21:19:51 +0530

    try

--- Amazon.test/src/test/java/com/testcases/TC_LoginPage_001.java
+++ Amazon.test/src/test/java/com/testcases/TC_LoginPage_001.java
@@ -1,29 +1,5 @@
 package com.testcases;
 
-import java.io.IOException;
-
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentTest;
-import com.pages.Base;
-import com.pages.LoginpageAmazon;
-
-
-public class TC_LoginPage_001 extends Base {
-	@Test
-	public void loginApps() throws InterruptedException, IOException 
-	{
-		
-		  logger.info("***  Starting TC_LoginPage_001    ***");
-		// logger=reports.createTest("login to Amazon");
-		 LoginpageAmazon loginpage=PageFactory.initElements(driver, LoginpageAmazon.class);
-		 logger.info("Starting Application");
-		 loginpage.LoginToAmazon( excel.getNumericData("Sheet1", 0, 0), excel.getStringData("Sheet1", 0, 1));
-		 logger.pass("Login Suceessfull");
-		
-		 
-		
-     }	
+public class TC_LoginPage_001 {
 
 }
